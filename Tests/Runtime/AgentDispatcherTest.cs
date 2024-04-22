4d8988375871d10a2e36971112b97eab5a1cb201
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-04-23 06:46:40 +0900

    Fix using directive

--- Tests/Runtime/AgentDispatcherTest.cs
+++ Tests/Runtime/AgentDispatcherTest.cs
@@ -3,10 +3,8 @@
 
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
-using Cysharp.Threading.Tasks.Triggers;
 using DeNA.Anjin.Agents;
 using DeNA.Anjin.Settings;
 using DeNA.Anjin.TestDoubles;
