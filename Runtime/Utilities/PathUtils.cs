8f6aa4b45087a124055b432dc653b61cc1f052f2
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-10-20 06:33:54 +0900

    Fix expression to work with Unity 2020 or lower

--- Runtime/Utilities/PathUtils.cs
+++ Runtime/Utilities/PathUtils.cs
@@ -16,10 +16,17 @@ namespace DeNA.Anjin.Utilities
         /// <param name="path">The file or directory for which to obtain absolute path information.</param>
         public static string GetAbsolutePath(string path)
         {
+#if UNITY_2021_2_OR_NEWER
             if (Path.IsPathFullyQualified(path))
             {
                 return path;
             }
+#else
+            if (Path.IsPathRooted(path)) // This is inaccurate but rarely a problem.
+            {
+                return path;
+            }
+#endif
 
             return Application.isEditor
                 ? Path.GetFullPath(path)
