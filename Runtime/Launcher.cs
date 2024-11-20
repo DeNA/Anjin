05b63389c3dcfca9145efcc40541ff30318d33c8
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-11-21 06:49:36 +0900

    Fix to support Unity 2020.2 or older

--- Runtime/Launcher.cs
+++ Runtime/Launcher.cs
@@ -224,7 +224,7 @@ namespace DeNA.Anjin
         {
             state.settings = null;
             state.exitCode = exitCode;
-#if UNITY_EDITOR
+#if UNITY_EDITOR && UNITY_2020_3_OR_NEWER
             AssetDatabase.SaveAssetIfDirty(state); // Note: Sync with virtual players of MPPM package
 #endif
 
